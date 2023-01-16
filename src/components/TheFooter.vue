12f365a469551c9096391db9b9b6544b95ea157a
黑白 <2285697543@qq.com>
2023-01-16 10:02:59 +0800

    新页面跳转

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,13 +3,13 @@
     <span class="opacity-75 mr-1.5">
       by
     </span>
-    <a class="text-teal-600" href="https://demo.civitasrise.com/">
+    <a class="text-teal-600" href="https://demo.civitasrise.com/" target="_blank">
       CivitasRise
     </a>
     <span class="opacity-75 mx-1.5">
       X
     </span>
-    <a class="text-teal-600" href="https://github.com/BlackWhite2000/CivitasRise-ProductivityCalculator">
+    <a class="text-teal-600" href="https://github.com/BlackWhite2000/CivitasRise-ProductivityCalculator" target="_blank">
       ProductivityCalculator
     </a>
   </div>
