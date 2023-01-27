4699e4edf765e87d6e844c3675c64c14d2796521
黑白 <2285697543@qq.com>
2023-01-27 16:00:55 +0800

    修复test流程

--- src/components/TheCounter.vue
+++ src/components/TheCounter.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  initial: number
+}>()
+const { count, inc, dec } = useCounter(props.initial)
+</script>
+
 <template>
-  <div />
+  <div class="hidden">
+    {{ count }}
+    <button class="inc" @click="inc()">
+      +
+    </button>
+    <button class="dec" @click="dec()">
+      -
+    </button>
+  </div>
 </template>
